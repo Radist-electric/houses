892c71d3ad634588bc05dc64f284bdd0843b07d8
Radist <radist-electric@yandex.ru>
2021-04-16 01:39:56 +0500

    I made authorization

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
@@ -16,40 +17,122 @@ const useStyles = makeStyles((theme: Theme) =>
       marginBottom: theme.spacing(3),
       maxWidth: 576,
       background: 'none'
+    },
+    input: {
+      marginBottom: theme.spacing(2)
     }
   }),
 )
 
+const initForm = [
+  {
+    label: 'Электронная почта',
+    error: false,
+    value: ''
+  },
+  {
+    label: 'Пароль',
+    error: false,
+    value: ''
+  }
+]
+
+const isLocalStorage = storageAvailable('localStorage')
+
+function storageAvailable(x: string) {
+  try {
+    localStorage.setItem(x, x)
+    localStorage.removeItem(x)
+    return true
+  }
+  catch (e) {
+    return false
+  }
+}
+
+const storageName: string = 'housesUser'
+let initUser: any
+if (isLocalStorage) {
+  initUser = JSON.parse(localStorage.getItem(storageName) || '{}')
+}
+console.log('initUser', initUser)
+
+
 export const AuthPage = () => {
   const classes = useStyles()
+  // const [user, setUser] = useState(initUser)
+  const [form, setForm] = useState(initForm)
 
   const formHandler = () => {
-    console.log('submitted')
-
+    console.log('submitted', form)
+    let isFormValid = true
+    const newForm = form.map((item) => {
+      item.error = item.value.trim().length === 0
+      isFormValid = !item.error && isFormValid
+      return item
+    })
+    setForm(newForm)
+    console.log('isFormValid', isFormValid)
+    if(isFormValid) {
+      loginHandler({username: form[0].value, password: form[1].value})
+      setForm(initForm)
+    }
   }
+
+  const loginHandler = async (loginData: {username:string, password: string}) => {
+    console.log('loginHandler', loginData)
+    try {
+      const url: string = 'http://test-alpha.reestrdoma.ru/api/login/'
+      const requestOptions = {
+        method: "POST", 
+        headers: {"Content-Type": "application/json" },
+        body: JSON.stringify(loginData)
+    }
+      console.log('requestOptions', requestOptions)
+      
+      const response = await fetch(url, requestOptions)
+      const data = await response.json()
+
+      if (!response.ok) {
+        console.log(data.errors.error[0])
+      }
+      console.log('data', data)
+      
+    } catch (e) {
+      throw e
+    }
+  }
+
+  const onChangeHandler = (event: any, index: any) => {
+    const input = { ...form[index] }
+    input.value = event.target.value
+    input.error = false
+    const newForm = [...form.slice(0, index), input, ...form.slice(index + 1)]
+    setForm(newForm)
+  }
+
+  const inputs = form.map((item, i) => {
+    return (
+      <TextField
+        key={i}
+        className={classes.input}
+        error={item.error}
+        required
+        label={item.label}
+        fullWidth={true}
+        variant='outlined'
+        value={item.value}
+        helperText={item.error && 'Поле не должно быть пустым'}
+        onChange={event => onChangeHandler(event, i)}
+      />
+    )
+  })
 
   return (
     <Paper className={classes.paper} elevation={0}>
       <Typography variant="h5" component="h1">Авторизация</Typography>
       <form className={classes.form} noValidate autoComplete="off">
-        <div>
-          <TextField
-            error={false}
-            id="outlined-error"
-            label="Электронная почта"
-            helperText="Поле не должно быть пустым"
-            variant="outlined"
-            fullWidth={true}
-          />
-          <TextField
-            error={false}
-            id="outlined-error-helper-text"
-            label="Пароль"
-            helperText="Поле не должно быть пустым"
-            variant="outlined"
-            fullWidth={true}
-          />
-        </div>
+        {inputs}
       </form>
       <Button
         variant='contained'
